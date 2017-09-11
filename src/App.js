c5ff5891a44f72cbc24eddd67245c6267c2659ff
amanshu-kataria <amanshukataria@hotmail.com>
2017-09-11 23:50:48 +0530

    Basic Layout

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import "react-bootstrap/dist/react-bootstrap.min.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Grid, Row, Col } from "react-bootstrap";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import AppBar from "material-ui/AppBar";
+import ToDoList from "./toDoList";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <MuiThemeProvider>
+        <Grid fluid className="noPadding">
+          <Row>
+            <Col xs={12} md={12}>
+              <AppBar
+                style={{
+                  backgroundColor: "#03A9F4"
+                }}
+                title="To-Do"
+                showMenuIconButton={false}
+              />
+            </Col>
+          </Row>
+          <Row className="target">
+            <Col xs={4} md={4}>
+              <ToDoList />
+            </Col>
+            <Col xs={8} md={8} />
+          </Row>
+        </Grid>
+      </MuiThemeProvider>
     );
   }
 }
