44bd2c22a5d1629ba0d9ab09251d4353153628d6
amanshu-kataria <amanshukataria@hotmail.com>
2017-09-12 12:53:31 +0530

    Functionality to display input for new task.

--- src/toDoList.js
+++ src/toDoList.js
@@ -6,25 +6,70 @@ import AppBar from "material-ui/AppBar";
 import "./toDoList.css";
 import ContentAdd from "material-ui/svg-icons/content/add";
 import IconButton from "material-ui/IconButton";
+import { List, ListItem } from "material-ui/List";
+import Divider from "material-ui/Divider";
+import TextField from "material-ui/TextField";
+import FlatButton from "material-ui/FlatButton";
+
+//Returns add button to the task bar
+function AddButton(props) {
+  return (
+    <IconButton onClick={props.onClick} iconStyle={{ color: props.color }}>
+      <ContentAdd />
+    </IconButton>
+  );
+}
 
 class ToDoList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      addTaskVisible: false
+    };
+    this.addInput = this.addInput.bind(this);
+  }
+
+  addInput(e) {
+    this.setState({ addTaskVisible: true });
+  }
+
   render() {
     const styles = {
       checkbox: {
-        marginLeft: 15,
-        marginTop: 16,
-        marginBottom: 16
+        paddingLeft: 16
+      },
+      textField: {
+        paddingLeft: 16,
+        marginRight: 20,
+        width: "70%"
       },
       defaultColor: {
         backgroundColor: "#67daff"
       }
     };
 
-    const addButton = (
-      <IconButton>
-        <ContentAdd />
-      </IconButton>
-    );
+    function Checkboxes() {
+      return (
+        <Checkbox
+          label="Label on the left"
+          labelPosition="left"
+          style={styles.checkbox}
+        />
+      );
+    }
+
+    function NewTask() {
+      return (
+        <div>
+          <TextField
+            hintText="Task"
+            style={styles.textField}
+            floatingLabelText="New Task"
+          />
+          <FlatButton label="Add" primary={true} />
+        </div>
+      );
+    }
 
     return (
       <div className="taskList">
@@ -32,19 +77,16 @@ class ToDoList extends Component {
           className="app-bar"
           style={styles.defaultColor}
           title="Task"
-          iconElementRight={addButton}
+          titleStyle={{ fontSize: 20 }}
+          iconElementRight={<AddButton onClick={this.addInput} color="white" />}
           showMenuIconButton={false}
         />
-        <Checkbox
-          label="Label on the left"
-          labelPosition="left"
-          style={styles.checkbox}
-        />
-        <Checkbox
-          label="Label on the left"
-          labelPosition="left"
-          style={styles.checkbox}
-        />
+        <div>
+          {this.state.addTaskVisible ? <NewTask /> : null}
+          <List>
+            <Checkboxes />
+          </List>
+        </div>
       </div>
     );
   }
